077575ac957413293481eb9e5a820a7e543627ed
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-04 23:22:58 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 function zeigeZeitRegister () {
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 1, 1, lcd16x2rgb.lcd16x2_text(bit.formatNumber(iReg, bit.eLength.HEX_F)))
     if (bit.between(iReg, 0, 6)) {
-    	
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 0, lcd16x2rgb.lcd16x2_text("smHdwMy".charAt(0)))
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 12, 13, lcd16x2rgb.lcd16x2_text(bit.formatNumber(rtcpcf85063tp.getDateTimeArray()[iReg], bit.eLength.HEX_FF)))
     }
 }
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
@@ -21,12 +23,14 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 function zeigeZeit () {
     if (bCLK) {
-    	
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 15, 15, lcd16x2rgb.lcd16x2_text(String.fromCharCode(233)))
     }
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 12, lcd16x2rgb.lcd16x2_text(rtcpcf85063tp.getDate(rtcpcf85063tp.ePart.mit, rtcpcf85063tp.ePart.mit)))
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 3, 11, lcd16x2rgb.lcd16x2_text(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit)))
     if (i25LED == 1) {
-    	
+        rtcpcf85063tp.anzeige25LED(rtcpcf85063tp.e25LED.Datum)
     } else if (i25LED == 2) {
-    	
+        rtcpcf85063tp.anzeige25LED(rtcpcf85063tp.e25LED.Zeit)
     }
 }
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
@@ -73,9 +77,9 @@ pins.onPulsed(DigitalPin.P2, PulseValue.Low, function () {
 function zeigeControlRegister () {
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 7, lcd16x2rgb.lcd16x2_text(bit.formatNumber(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.Control_1), bit.eLength.BIN_11111111)))
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 7, lcd16x2rgb.lcd16x2_text(bit.formatNumber(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.Control_2), bit.eLength.BIN_11111111)))
-    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 10, 14, lcd16x2rgb.lcd16x2_text(bit.formatNumber(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.RAM_byte), bit.eLength.HEX_FF)))
-    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 13, 14, lcd16x2rgb.lcd16x2_text(bit.formatNumber(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.Offset), bit.eLength.HEX_FF)))
-    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 9, 11, bit.sign(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.Offset), 6))
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 10, 14, lcd16x2rgb.lcd16x2_text(bit.formatNumber(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.RAM_byte), bit.eLength.HEX_FF)), lcd16x2rgb.eAlign.right)
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 13, 14, lcd16x2rgb.lcd16x2_text(bit.formatNumber(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.Offset), bit.eLength.HEX_FF)), lcd16x2rgb.eAlign.right)
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 9, 11, bit.sign(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.Offset), 6), lcd16x2rgb.eAlign.right)
 }
 function initRGB () {
     lcd16x2rgb.initRGB(lcd16x2rgb.lcd16x2rgb_eADDR(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5))
@@ -106,14 +110,19 @@ input.onButtonEvent(Button.B, ButtonEvent.Hold, function () {
 })
 input.onButtonEvent(Button.AB, ButtonEvent.Hold, function () {
     if (iStatus == 1) {
+        rtcpcf85063tp.initRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
+        lcd16x2rgb.clearScreen(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2))
+        lcd16x2rgb.setDisplay(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), true, true, true)
         basic.setLedColor(0x0000ff)
         iStatus = 2
         zeigeZeit()
         zeigeZeitRegister()
     } else if (iStatus == 2) {
+        lcd16x2rgb.clearScreen(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2))
+        lcd16x2rgb.setDisplay(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), true, true, true)
         basic.setLedColor(0xffff00)
         iStatus = 3
-        iOffset = bit.sign(0, 6)
+        iOffset = bit.sign(rtcpcf85063tp.readRegister(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.eControl.Offset), 6)
         zeigeControlRegister()
     }
 })
